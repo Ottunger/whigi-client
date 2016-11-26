a46cd6da2cc4fa8a537392e952fbffb7599fe841
Grégoire Mathonet <greg.mathonet@gmail.com>
2016-11-26 18:22:38 +0100

    Keep _ rather than .

--- app/subcmpts/logging.component.ts
+++ app/subcmpts/logging.component.ts
@@ -309,7 +309,7 @@ export class Logging implements OnInit {
             window.$('.iuname').addClass('has-error');
             return;
         }
-        if(/[^a-zA-Z0-9\-\.]/.test(this.username)) {
+        if(/[^a-zA-Z0-9\-\_]/.test(this.username)) {
             self.notif.error(self.translate.instant('error'), self.translate.instant('login.badChars'));
             window.$('.iuname').addClass('has-error');
             return;
